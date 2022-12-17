115ccbe3304e425bb0cd75a7620b0ffca08dd1ed
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-12-17 11:21:22 +0300

    Update "upload_restaurants" commands.

--- foodcartapp/management/commands/upload_restaurants.py
+++ foodcartapp/management/commands/upload_restaurants.py
@@ -1,15 +1,38 @@
 import json
 
-from pathlib import Path
-
 from django.core.management.base import BaseCommand
 
-def main():
-    print('OK')
+from foodcartapp.models import Restaurant
 
 
 class Command(BaseCommand):
-    help = 'Start adding dishes'
+    help = 'Start adding restaurants'
 
     def handle(self, *args, **options):
-        main()
+
+        with open(options['path'], 'r', encoding='utf-8') as file:
+            restaurants = json.load(file)
+
+        for restaurant in restaurants:
+            self.add_restaurant(restaurant)
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'path',
+            nargs='?',
+            type=str,
+        )
+
+    def add_restaurant(self, restaurant_notes: dict):
+        restaurant, created = Restaurant.objects.get_or_create(
+            name=restaurant_notes['title'],
+            defaults={
+                'address': restaurant_notes.get('address', ''),
+                'contact_phone': restaurant_notes.get('contact_phone', ''),
+            }
+        )
+
+        if created:
+            self.stdout.write(f'Added restaurant "{restaurant}".')
+        else:
+            self.stdout.write(f'\033[93mDOUBLE:\033[0m restaurant "{restaurant}" already exists!')
