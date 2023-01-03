9e011a8311a9cece17580406450ad2151913d46b
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-01-03 16:54:24 +0300

    List orders info manager UI.

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -8,8 +8,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 
 
-from foodcartapp.models import Product, Restaurant
-
+from foodcartapp.models import Product, Restaurant, Order
+from foodcartapp.views import OrderSerializer
 
 class Login(forms.Form):
     username = forms.CharField(
@@ -93,5 +93,5 @@ def view_restaurants(request):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     return render(request, template_name='order_items.html', context={
-        # TODO заглушка для нереализованного функционала
+        'orders': [OrderSerializer(order).data for order in Order.objects.all()]
     })
