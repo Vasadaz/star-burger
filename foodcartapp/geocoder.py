06423882936da52e95e597e6a8dbd689b01e1ab7
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-03-06 22:43:41 +0300

    Optimized the "fetch_coordinates" function.

--- foodcartapp/geocoder.py
+++ foodcartapp/geocoder.py
@@ -5,13 +5,19 @@ from django.conf import settings
 
 def fetch_coordinates(address: str) -> tuple[float, float] | None:
     base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params=dict(geocode=address, apikey=settings.YANDEX_GEO_API, format="json"))
-    response.raise_for_status()
-
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
 
     try:
+        response = requests.get(base_url, params=dict(geocode=address, apikey=settings.YANDEX_GEO_API, format="json"))
+        response.raise_for_status()
+        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
         most_relevant = found_places[0]
+
+    except requests.exceptions.ConnectionError:
+        return None
+
+    except requests.exceptions.HTTPError:
+        return None
+
     except IndexError:
         return None
 
